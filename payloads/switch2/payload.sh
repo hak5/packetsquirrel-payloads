08edd1610a70c8b7bb43520709b78a29b028c32c
Martin Sundhaug <martinsundhaug@gmail.com>
2017-10-21 08:52:34 +0200

    Add PBP-metas for the dummy

--- payloads/switch2/payload.sh
+++ payloads/switch2/payload.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# 
+# Title:	    default
+# Description:  Sets the NETMODE to NAT, then sets the LED to ATTACK
+# Author: 	    SebKinne
+# Version:	    1.0
+# Category:     default
+# Target: 	    Any
+# Net Mode:	    NAT
 
 NETMODE NAT
 LED ATTACK
